c7a25f936562e95b7b4b021adaf895fcb0118366
Antonio Rice <ricex407@gmail.com>
2023-12-11 14:19:22 -0500

    LC 104 - preOrder DFS using a Stack

--- maxDepth.js
+++ maxDepth.js
@@ -20,7 +20,7 @@ function TreeNode(val, left, right) {
   this.right = right === undefined ? null : right;
 }
 
-// recursive
+//// recursive - DFS
 // var maxDepth = function (root) {
 //   if (root == null) return 0;
 
@@ -30,33 +30,56 @@ function TreeNode(val, left, right) {
 //   return Math.max(left, right) + 1; // adding one to act out a counter as we traverse down
 // };
 
-//Bredth First Search uses a queue. Adds each level's children to queue and keep track of level count
+//// Bredth First Search uses a queue. Adds each level's children to queue and keep track of level count
+// var maxDepth = function (root) {
+//   if (root == null) return 0;
+//   let queue = [];
+//   let level = 0;
+
+//   // add root to queue
+//   queue.push(root);
+
+//   //loop through queue
+//   while (queue.length) {
+//     // loop through each item in queue (child level)
+//     for (let i = 0; i < queue.length; i++) {
+//       let node = queue.shift();
+//       // add left and right nodes to queue to be checked for children
+//       if (node.left != null) {
+//         queue.push(node.left);
+//       }
+//       if (node.right != null) {
+//         queue.push(node.right);
+//       }
+//     }
+//     // after each check, add the levels
+//     level += 1;
+//   }
+
+//   return level;
+// };
+
+// pre-Order DFS uses a stack
 var maxDepth = function (root) {
-  if (root == null) return 0;
-  let queue = [];
-  let level = 0;
+  // add node to stack with its depth
+  const stack = [[root, 1]];
+  let maxLevel = -Infinity;
 
-  // add root to queue
-  queue.push(root);
+  // loop through the stack, removing the parent, and adding it children to the stack
+  while (stack.length) {
+    let [node, depth] = stack.pop();
 
-  //loop through queue
-  while (queue.length) {
-    // loop through each item in queue (child level)
-    for (let i = 0; i < queue.length; i++) {
-      let node = queue.shift();
-      // add left and right nodes to queue to be checked for children
-      if (node.left != null) {
-        queue.push(node.left);
-      }
-      if (node.right != null) {
-        queue.push(node.right);
-      }
+    // add left and right child to stack to continue the process, and keeping track of the depth
+    if (node != null) {
+      maxLevel = Math.max(maxLevel, depth);
+      stack.push([node.left, depth + 1]);
+      stack.push([node.right, depth + 1]);
     }
-    // after each check, add the levels
-    level += 1;
   }
 
-  return level;
+  // once the leafs reach null, the stack with be empty, return observed max level
+
+  return maxLevel;
 };
 
 const tree1 = new TreeNode(
