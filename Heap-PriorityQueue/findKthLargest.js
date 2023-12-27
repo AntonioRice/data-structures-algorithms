e18e591643ea2914a08b64d5be20a35b8a8b5afc
Antonio Rice <ricex407@gmail.com>
2023-12-27 15:34:02 -0500

    LC 215

--- Heap-PriorityQueue/findKthLargest.js
+++ Heap-PriorityQueue/findKthLargest.js
@@ -1,3 +1,4 @@
+const Heap = require("./Heap");
 /**
  * Given an integer array nums and an integer k, return the kth largest element in the array.
  * Note that it is the kth largest element in the sorted order, not the kth distinct element.
@@ -7,29 +8,43 @@
  * @return {number}
  */
 
-// using quick sort -> O(nlogn)
+// // using quick sort -> O(nlogn)
+// var findKthLargest = function (nums, k) {
+//   const sortedArr = quickSort(nums); // O(nlogn)
+
+//   return sortedArr[sortedArr.length - k]; // O(1)
+
+//   function quickSort(arr) {
+//     if (arr.length < 2) return arr;
+//     let pivot = arr[arr.length - 1];
+//     let left = [];
+//     let right = [];
+
+//     for (let i = 0; i < arr.length - 1; i++) {
+//       if (arr[i] < pivot) {
+//         left.push(arr[i]);
+//       } else {
+//         right.push(arr[i]);
+//       }
+//     }
+
+//     return [...quickSort(left), pivot, ...quickSort(right)];
+//   }
+// };
+
 var findKthLargest = function (nums, k) {
-  const sortedArr = quickSort(nums); // O(nlogn)
-
-  return sortedArr[sortedArr.length - k]; // O(1)
-
-  function quickSort(arr) {
-    if (arr.length < 2) return arr;
-    let pivot = arr[arr.length - 1];
-    let left = [];
-    let right = [];
-
-    for (let i = 0; i < arr.length - 1; i++) {
-      if (arr[i] < pivot) {
-        left.push(arr[i]);
-      } else {
-        right.push(arr[i]);
-      }
-    }
-
-    return [...quickSort(left), pivot, ...quickSort(right)];
+  const numHeap = new Heap();
+  for (let i = 0; i < nums.length; i++) {
+    numHeap.insert(nums[i]);
   }
+
+  for (let i = 1; i < k; i++) {
+    numHeap.remove();
+  }
+
+  return numHeap.remove();
 };
 
-console.log(findKthLargest([3, 2, 1, 5, 6, 4], 2));
-console.log(findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4));
+console.log(findKthLargest([3, 2, 1, 5, 6, 4], 2)); //5
+console.log(findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4)); //4
+console.log(findKthLargest([1], 1)); // 1
