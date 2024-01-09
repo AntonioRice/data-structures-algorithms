726b7d035837af3202a3ea8f513b802478e766ed
Antonio Rice <ricex407@gmail.com>
2024-01-09 16:28:24 -0500

    mergeSort

--- Sorting/mergeSort.js
+++ Sorting/mergeSort.js
@@ -3,6 +3,7 @@
  * Split array into sub arrays byt dividing by 2, sub array until each contains only one element (sorted).
  * Using a temp array, repeatedly merge sub arrays until only one remains.
  * Big O: Recursively dividing array into halves is O(logn). Merging array while while loop is O(n) ==> O(nlogn)
+ * Space: 0(n)
  */
 
 const mergeSort = (arr) => {
@@ -10,24 +11,24 @@ const mergeSort = (arr) => {
     return arr;
   }
   const mid = Math.floor(arr.length / 2);
-  const leftArr = arr.slice(0, mid);
-  const rightArr = arr.slice(mid);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
 
   //repeat if length of left and right arrays aren't one
-  return merge(mergeSort(leftArr), mergeSort(rightArr));
+  return merge(mergeSort(left), mergeSort(right));
 };
 
-const merge = (leftArr, rightArr) => {
-  const sortedArr = [];
-  while (leftArr.length && rightArr.length) {
-    if (leftArr[0] <= rightArr[0]) {
-      sortedArr.push(leftArr.shift()); //remember that arr.shift remove the element from arr
+const merge = (left, right) => {
+  const sorted = [];
+  while (left.length && right.length) {
+    if (left[0] <= right[0]) {
+      sorted.push(left.shift()); //remember that arr.shift remove the element from arr
     } else {
-      sortedArr.push(rightArr.shift());
+      sorted.push(right.shift());
     }
   }
 
-  return [...sortedArr, ...leftArr, ...rightArr];
+  return [...sorted, ...left, ...right];
 };
 
 const arr = [-6, 20, 8, -2, 4]; // return [-6, -2, 4, 8, 20]
