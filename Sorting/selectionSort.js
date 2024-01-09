a97801aee4d65ca634ceca78610dabcbb27e170a
Antonio Rice <ricex407@gmail.com>
2024-01-09 16:14:58 -0500

    selection sort

---
+++ Sorting/selectionSort.js
@@ -0,0 +1,16 @@
+const selectionSort = () => {
+  for (let i = 0; i < arr.length; i++) {
+    let min = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[min]) {
+        min = j;
+      }
+    }
+    [arr[i], arr[min]] = [arr[min], arr[i]];
+  }
+
+  return arr;
+};
+
+const arr = [0, 8, 51, 28, 3, 4, 99, 100];
+console.log(selectionSort(arr));
