a40a23de1699d6b4aa13f41c9ee01cbc898a8b28
Antonio Rice <ricex407@gmail.com>
2024-01-09 16:14:13 -0500

    fibb update

--- fibonacci.js
+++ fibonacci.js
@@ -1,26 +1,17 @@
 function fibonacci(n) {
+  // Big-O 0(n) - linear
   const fib = [0, 1];
-
-  for (let i = 2; i < n; i++) {
+  for (let i = 2; i <= n; i++) {
     fib[i] = fib[i - 1] + fib[i - 2];
   }
-  return fib;
-}
+  return fib[n];
 
-function fibonacci(n) {
-  // Big-O 0(n) - linear
-  //   const fib = [0, 1];
-  // for (let i = 2; i < n; i++) {
-  //   fib[i] = fib[i - 1] + fib[i - 2];
+  // // Big-O O(2^n)
+  // if (n < 2) {
+  //   return n;
+  // } else {
+  //   return fibonacci(n - 1) + fibonacci(n - 2);
   // }
-  // return fib;
-
-  // Big-O O(2^n)
-  if (n < 2) {
-    return n;
-  } else {
-    return fibonacci(n - 1) + fibonacci(n - 2);
-  }
 }
 
 console.log(fibonacci(1)); //[0,1]
