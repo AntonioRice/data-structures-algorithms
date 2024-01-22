92374be89196b2269ea2984d733aa973a5afc162
Antonio Rice <ricex407@gmail.com>
2024-01-22 15:26:33 -0500

    trie update

--- Trie/Trie.js
+++ Trie/Trie.js
@@ -24,6 +24,7 @@ class Trie {
         node = node[c];
       } else {
         node = null;
+        break;
       }
     }
 
@@ -31,21 +32,25 @@ class Trie {
   }
   search(word) {
     let node = this.traverse(word);
-    return node !== null && node.isWord === true;
+    return node != null && node.isWord == true;
   }
   startsWith(prefix) {
     let node = this.traverse(prefix);
-    return node !== null;
+    return node != null;
   }
 }
 
-const trie = new Trie();
+// const trie = new Trie();
+// trie.insert("hotdog");
+// console.log(trie.startsWith("dog"));
+// trie.insert("apple");
+// trie.insert("apples");
+// trie.insert("car");
+// trie.insert("carpet");
+// console.log(trie.search("carpet"));
+// console.log(trie.startsWith("car"));
+// console.log(trie.startsWith("carx"));
+// console.log(trie.search("a"));
+// console.log(JSON.stringify(trie));
 
-console.log(trie.insert("car"));
-console.log(trie.insert("carpet"));
-console.log(trie.search("car"));
-console.log(trie.search("cars"));
-console.log(trie.search("car"));
-console.log(trie.search("carpet"));
-console.log(trie.startsWith("carpet"));
-console.log(JSON.stringify(trie));
+module.exports = Trie;
