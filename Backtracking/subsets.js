58162e82200b9736829f5319b88d92ccc1250874
Antonio Rice <ricex407@gmail.com>
2024-01-23 13:02:15 -0500

    LC 78

--- Backtracking/subsets.js
+++ Backtracking/subsets.js
@@ -1,4 +1,7 @@
 /**
+ * LC 78
+ * Given an integer array nums of unique elements, return all possible subsets (the power set).
+ * The solution set must not contain duplicate subsets. Return the solution in any order.
  * @param {number[]} nums
  * @return {number[][]}
  */
