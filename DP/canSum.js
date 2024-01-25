8fc9e33e13d0beddec726b4c6cbbb90827be188f
Antonio Rice <ricex407@gmail.com>
2024-01-25 12:33:57 -0500

    canSum dp memo

---
+++ DP/canSum.js
@@ -0,0 +1,44 @@
+/**
+ * canSum: given an array of integers and a target interger, can a the target be reached given the nums provided.
+ * Use any num as much as possible, non negative assumption
+ */
+
+// Brute force
+// Time: O(n^m), Space(m) (height of the tree)
+// m = target sum, n => array length
+const canSum = (numbers, targetSum) => {
+  if (targetSum === 0) return true;
+  if (targetSum < 0) return false;
+
+  for (const num of numbers) {
+    const remainder = targetSum - num;
+    const isPossible = canSum(numbers, remainder);
+
+    if (isPossible) return true;
+  }
+
+  return false;
+};
+
+// console.log(canSum([5, 3, 4, 7], 7)); // true =>
+// console.log(canSum([5, 3], 7)); // false
+
+//Time: O(m*n), Space O(m)
+const memoCanSum = (numbers, targetSum, memo = {}) => {
+  if (targetSum in memo) return memo[targetSum];
+  if (targetSum === 0) return true;
+  if (targetSum < 0) return false;
+
+  for (const num of numbers) {
+    const remainder = targetSum - num;
+    memo[targetSum] = memoCanSum(numbers, remainder, memo);
+
+    if (memo[targetSum]) return true;
+  }
+
+  return (memo[targetSum] = false);
+};
+console.log(memoCanSum([5, 3, 4, 7], 7)); // true =>
+console.log(memoCanSum([5, 3], 7)); // false
+console.log(memoCanSum([5, 1], 7)); // true
+console.log(memoCanSum([14, 7], 300)); // true
