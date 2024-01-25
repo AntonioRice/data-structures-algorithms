130185241f1c8ec5ab2918ce916fc085234e33fc
Antonio Rice <ricex407@gmail.com>
2024-01-25 15:54:02 -0500

    all combinations dp

--- DP/canConstruct.js
+++ DP/canConstruct.js
@@ -1,5 +1,5 @@
 /**
- * @param {string[]} word
+ * @param {string[]} words
  * @return {boolean}
  */
 
@@ -47,14 +47,18 @@ const memoCanConstruct = (words, target, memo = {}) => {
   return false;
 };
 
-console.log(memoCanConstruct(["ab", "abc", "cd", "def"], "abcd")); //true
-console.log(
-  memoCanConstruct(
-    ["e", "ee", "eeee", "eeeee", "eeeeee", "eeeeeeeeeeeeeeeee"],
-    "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef"
-  )
-); //false
+// console.log(memoCanConstruct(["ab", "abc", "cd", "def"], "abcd")); //true
+// console.log(
+//   memoCanConstruct(
+//     ["e", "ee", "eeee", "eeeee", "eeeeee", "eeeeeeeeeeeeeeeee"],
+//     "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef"
+//   )
+// ); //false
 
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
 const memoCanConstructCount = (words, target, memo = {}) => {
   if (target in memo) return memo[target];
   if (target === "") return 1;
@@ -73,11 +77,36 @@ const memoCanConstructCount = (words, target, memo = {}) => {
   return count;
 };
 
-console.log(memoCanConstructCount(["ab", "abc", "cd", "def"], "abcd")); //1
-console.log(memoCanConstructCount(["purp", "p", "ur", "le", "purpl"], "purple")); //2
-console.log(
-  memoCanConstructCount(
-    ["e", "ee", "eeee", "eeeee", "eeeeee", "eeeeeeeeeeeeeeeee"],
-    "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef"
-  )
-); //0
+// console.log(memoCanConstructCount(["ab", "abc", "cd", "def"], "abcd")); //1
+// console.log(memoCanConstructCount(["purp", "p", "ur", "le", "purpl"], "purple")); //2
+// console.log(
+//   memoCanConstructCount(
+//     ["e", "ee", "eeee", "eeeee", "eeeeee", "eeeeeeeeeeeeeeeee"],
+//     "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef"
+//   )
+// ); //0
+
+// Time: O(n^m), Space: O(m)
+//m = target.length, n = words.length
+const allCanConstruct = (words, target) => {
+  if (target === "") return [[]];
+
+  let allCombinations = [];
+
+  for (let word of words) {
+    if (target.indexOf(word) === 0) {
+      const suffix = target.slice(word.length); // * m to time
+      const suffixWays = allCanConstruct(words, suffix);
+      const targetWays = suffixWays.map((way) => [word, ...way]);
+
+      allCombinations.push(...targetWays);
+    }
+  }
+
+  return allCombinations;
+};
+
+console.log(allCanConstruct(["ab", "abc", "cd", "def"], "abcd"));
+// console.log(
+//   allCanConstruct(["e", "ee", "eeee", "eeeee", "eeeeee", "eeeeeeeeeeeeeeeee"], "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef")
+// );
