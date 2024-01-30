01f8307daaf1fb47df3182a20ee4294ddb011753
Antonio Rice <ricex407@gmail.com>
2024-01-30 17:27:48 -0500

    LC 88 - pointers approach

--- Array-Strings/merge.js
+++ Array-Strings/merge.js
@@ -30,9 +30,30 @@ var merge = function (nums1, m, nums2, n) {
   return nums1;
 };
 
+// Using Pointers. Time: O(n + m), Space O(1)
+var merge2 = function (nums1, m, nums2, n) {
+  let i = m - 1;
+  let j = n - 1;
+  let k = m + n - 1;
+
+  while (j >= 0) {
+    if (i >= 0 && nums1[i] > nums2[j]) {
+      nums1[k] = nums1[i];
+      k--;
+      i--;
+    } else {
+      nums1[k] = nums2[j];
+      k--;
+      j--;
+    }
+  }
+
+  return nums1;
+};
+
 const nums1 = [4, 5, 6, 0, 0, 0];
 const m = 3;
 const nums2 = [1, 2, 3];
 const n = 3;
 
-console.log(merge(nums1, m, nums2, n)); // [1,2,3,4,5,6]
+console.log(merge2(nums1, m, nums2, n)); // [1,2,3,4,5,6]
