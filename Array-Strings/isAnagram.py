a09bcf231c793c80ea814c9ff3fe5359c9fb9b9e
AntonioRice <ricex407@gmail.com>
2024-11-17 12:01:24 -0500

    LC 242 hashmap

--- Array-Strings/isAnagram.py
+++ Array-Strings/isAnagram.py
@@ -16,10 +16,27 @@
 # Time: Overall O(n)
 # Space O(1)
 #
-from collections import Counter
+# from collections import Counter
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:        
+#         return Counter(s) == Counter(t)
+
+# HashMap (counting frequency)
+# Time: O(n)
+# Space: O(n)
 class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:        
-        return Counter(s) == Counter(t)
+    def isAnagram(self, s: str, t: str) -> bool:       
+        if len(s) != len(t): return False 
+        counter = {}
+        
+        for char in s:
+            counter[char] = counter.get(char, 0) + 1
+        for char in t:
+            counter[char] = counter.get(char, 0) - 1 
+            if counter[char] < 0:
+                return False
+            
+        return True
     
 solution = Solution()
 print(solution.isAnagram("anagram","nagaram"))
