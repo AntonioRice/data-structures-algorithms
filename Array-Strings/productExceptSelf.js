d06331e3e8525db0c1e6d7a2d0cff1aa6962c888
AntonioRice <ricex407@gmail.com>
2024-11-18 12:53:31 -0500

    lc 238

--- Array-Strings/productExceptSelf.js
+++ Array-Strings/productExceptSelf.js
@@ -1,4 +1,5 @@
 /**
+ * LC 238
  * Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
  * The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
  * You must write an algorithm that runs in O(n) time and without using the division operation.
@@ -21,6 +22,25 @@ var productExceptSelf = function (nums) {
 
   return ans;
 };
+
+// const productExceptSelf = (list) => {
+//   const res = new Array(list.length).fill(1);
+//   let prefix = 1;
+//   for (let i = 0; i < list.length; i++) {
+//     res[i] = prefix;
+//     prefix *= list[i];
+//   }
+
+//   let suffix = 1;
+//   for (let i = list.length - 1; i >= 0; i--) {
+//     res[i] *= suffix;
+//     suffix *= list[i];
+//   }
+
+//   return res;
+// };
+
+// console.log(productExceptSelf([1, 2, 4, 6]));
 
 const res = productExceptSelf([1, 2, 3, 4]);
 const exp = [24, 12, 8, 6];
---
+++ Array-Strings/productExecptSelf.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        res = [1] * n        
+        
+        prefix = 1
+        for i in range(n):
+            res[i] = prefix
+            prefix *= nums[i]
+        
+        suffix = 1
+        for i in range(n-1, -1, -1):
+            res[i] *= suffix
+            suffix *= nums[i]
+        
+        return res
+
+solution = Solution()
+print(solution.productExceptSelf([1,2,4,6]))
