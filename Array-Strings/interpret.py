dabbf56cda0526f2debe8d9290049b660fc08fc2
AntonioRice <ricex407@gmail.com>
2024-11-21 14:22:37 -0500

    LC 1678

--- Array-Strings/interpret.py
+++ Array-Strings/interpret.py
@@ -1,7 +1,27 @@
-# LC 1678
+## LC 1678
+# class Solution:
+#     def interpret(self, command: str) -> str:
+#         return command.replace("()","o").replace("(al)","al")
+
+
+# Overall Time & Space O(n)
 class Solution:
     def interpret(self, command: str) -> str:
-        return command.replace("()","o").replace("(al)","al")
+        map = {"()":"o","(al)":"al"} # O(k = 2) -> O(1) fixed
+        res = [] # O(n) grows
+        key = "" # O(1)
+        
+        for i in range(len(command)): # O(n)
+            if(command[i] == "(" or len(key) > 0):
+                key += command[i]
+                
+                if(key in map): # O(1)
+                    res.append(map[key])
+                    key = ""
+            else:
+                res.append(command[i]) # O(1)
+                
+        return "".join(res) # O(n)
     
 solution = Solution()
 print(solution.interpret("G()(al)"))
